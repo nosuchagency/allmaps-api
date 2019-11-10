2d3d9eba7aa2e336def6c91afa604f2706069a60
Victor-Emil <victoremil.r.andersen@gmail.com>
2019-11-10 13:45:33 +0100

    aesthetics

--- app/Rules/SkinZipFileRule.php
+++ app/Rules/SkinZipFileRule.php
@@ -26,8 +26,8 @@ class SkinZipFileRule implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string $attribute
-     * @param  mixed $value
+     * @param string $attribute
+     * @param mixed $value
      *
      * @return bool
      */
