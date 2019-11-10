c3174850073c69b7f3d2f0730b12fdbbd8264119
Victor-Emil <victoremil.r.andersen@gmail.com>
2019-11-10 10:11:24 +0100

    only allow two locales and make locale default to app locale

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('locale')->nullable();
+            $table->string('locale')->default(config('app.locale'));
 
             $table->text('description')->nullable();
 
